0ca397a2bde44224fc9b90e99079232ea86076b7
LinhPT <ptuanlinh95@gmail.com>
2025-08-24 20:55:49 +0700

    init project

---
+++ src/test/java/org/example/components/ComponentsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.components;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
